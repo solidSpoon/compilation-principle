0a9c21bda6450ecfccfab10050a6be76df134912
cedarrival <cedarrival@foxmail.com>
2022-06-08 17:47:15 +0800

    init

---
+++ src/main/java/xyz/solidspoon/lexer/interf/Token.java
@@ -0,0 +1,23 @@
+package xyz.solidspoon.lexer.interf;
+
+import xyz.solidspoon.lexer.eum.TokenType;
+
+/**
+ * 一个简单的Token。
+ * 只有类型和文本值两个属性。
+ */
+public interface Token{
+
+    /**
+     * Token的类型
+     * @return
+     */
+    public TokenType getType();
+
+    /**
+     * Token的文本值
+     * @return
+     */
+    public String getText();
+
+}
---
+++ src/main/java/xyz/solidspoon/lexer/interf/impl/SimpleToken.java
@@ -0,0 +1,28 @@
+package xyz.solidspoon.lexer.interf.impl;
+
+import xyz.solidspoon.lexer.eum.TokenType;
+import xyz.solidspoon.lexer.interf.Token;
+
+/**
+ *  Token的一个简单实现。只有类型和文本值两个属性。
+ */
+public final class SimpleToken implements Token {
+
+
+        //Token类型
+        public TokenType type = null;
+
+        //文本值
+        public String text = null;
+
+
+        @Override
+        public TokenType getType() {
+            return type;
+        }
+
+        @Override
+        public String getText() {
+            return text;
+        }
+    }
